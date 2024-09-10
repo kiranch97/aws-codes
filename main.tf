e47d7a2d51d1528089800c217c810a20031c8c66
kiranch97 <kiranch97@gmail.com>
2024-09-10 13:58:10 +0000

    changes in keypair name

--- main.tf
+++ main.tf
@@ -90,7 +90,7 @@ resource "aws_instance" "my_ec2_instance" {
   ami           = "ami-0c55b159cbfafe1f0"  # Change to the latest AMI ID in your region
   instance_type = "t2.micro"
  
-  key_name               = aws_key_pair.kiran-keypair.key_name
+  key_name               = "kiran-keypair"
   subnet_id              = aws_subnet.my_subnet.id
   vpc_security_group_ids = [aws_security_group.my_sg.id]
  
